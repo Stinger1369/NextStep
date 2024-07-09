5e78ab8aea253146dfce3100e69ca9f399714b41
bilel zaaraoui <72788978+Stinger1369@users.noreply.github.com>
2024-07-09 13:30:58 +0200

    2024-07-09-13h30-bilel--------

--- nextstepsocket/data/.bash_history
+++
@@ -1,8 +0,0 @@
-ls /docker-entrypoint-initdb.d/
-exit
-mongosh --version
-mongosh mychatdb /docker-entrypoint-initdb.d/init-mongo.js
-exit
-mongosh admin --username root --password rootpassword /docker-entrypoint-initdb.d/init-mongo.js
-mongosh --username root --password rootpassword
-exit
--- nextstepsocket/src/main/java/com/example/mychat/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.mychat.repository;
-
-import com.example.mychat.model.User;
-import org.bson.types.ObjectId;
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-
-public interface UserRepository extends ReactiveMongoRepository<User, ObjectId> {
-}
